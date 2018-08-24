1b926160de3665c44305c3b127de38eef5fd691c
Omaro Taylor <smart.omarodev@gmail.com>
2018-08-23 21:13:46 -0500

    create wallet screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,215 +1,25 @@
 <template>
   <div>
-    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item">
-          <img src="~/assets/img/verto-dark.png" alt="A multi-currency crypto wallet with initial support for EOS &amp; VTX">
-        </a>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-start">
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Wallet
-            </a>
-
-            <div class="navbar-dropdown">
-              <a class="navbar-item" @click="createWallet">
-                Create
-              </a>
-              <a class="navbar-item" @click="listWallets">
-                List
-              </a>
-              <a class="navbar-item" @click="openWallet">
-                Open
-              </a>
-              <a class="navbar-item" @click="unlockWallet">
-                Unlock
-              </a>
-            </div>
-          </div>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Keys
-            </a>
-
-            <div class="navbar-dropdown">
-              <a class="navbar-item" @click="listKeyPairs">
-                List key pairs
-              </a>
-              <a class="navbar-item" @click="listPublicKeys">
-                List public keys
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="navbar-end">
-          <div v-if="wallet" class="navbar-item">
-            Wallet: {{ wallet.name }}
-          </div>
-          <div class="navbar-item">
-            0 VTX
-          </div>
-        </div>
-      </div>
-    </nav>
-
-    <section v-if="wallet" class="section">
-      <div class="container">
-        <qr :size="200" :text="wallet.publicKey" class="has-text-centered"/>
-      </div>
-      <div class="container">
-        <div class="has-text-centered">
-          Your public key: {{ wallet.publicKey }}
-        </div>
-      </div>
-      <div class="container">
-        <div>
-          <label class="label">URL to EOS node</label>
-          <input v-model="eosUrl" class="input" type="url" size="100">
-          <button class="button" @click="saveEosUrl">Save</button>
-        </div>
-      </div>
-    </section>
-
-    <footer class="fixed-footer">
-      <textarea id="messages" v-model="messages" class="textarea code" rows="8" type="text" readonly/>
-    </footer>
+    <div class="container is-fluid">
+      {{ messages }}
+    </div>
   </div>
 </template>
 
 <script>
-import qr from "vue-qr";
-
-const KEY_WALLET = "wallet";
-const KEY_EOS_URL = "eos_url";
-
-const SERVICE_VERTO = "verto.volentix.io";
-
-const DEFAULT_URL = "http://127.0.0.1:8888";
-
-import Store from "electron-store";
-import keytar from "keytar";
-
-const store = new Store({
-  encryptionKey: "j6=NPbpu#i&4=]u+xv_s8a'f^F}Y{ae_h2]Q=]*B"
-});
-
 export default {
-  components: {
-    qr
-  },
   data() {
     return {
-      messages: "",
-      wallet: null, // { name: "wallet_xxx", publicKey: "EOS..."" }
-      eosUrl: null
+      messages: "Welcome page",
+      is_login: false
     };
   },
-  mounted() {
-    this.wallet = store.get(KEY_WALLET, null);
-    this.eosUrl = store.get(KEY_EOS_URL, DEFAULT_URL);
-  },
-  methods: {
-    async createWallet() {
-      // TODO Allow the user to set the wallet name
-      this.wallet.name = "my_wallet_" + getRandomInt(1000);
-      const password = await this.invoke(
-        "/v1/wallet/create",
-        JSON.stringify(this.wallet.name)
-      );
-      await keytar.setPassword(SERVICE_VERTO, this.wallet.name, password);
-
-      await this.openWallet();
-      await this.unlockWallet();
-      const publicKey = await this.createKeys();
-
-      this.wallet = {
-        name: this.wallet.name,
-        publicKey
-      };
-      store.set(KEY_WALLET, this.wallet);
-    },
-    async listWallets() {
-      return this.invoke("/v1/wallet/list_wallets");
-    },
-    async listPublicKeys() {
-      return this.invoke("/v1/wallet/get_public_keys");
-    },
-    async listKeyPairs() {
-      return this.invoke("/v1/wallet/list_keys");
-    },
-    async openWallet() {
-      return this.invoke("/v1/wallet/open", JSON.stringify(this.wallet.name));
-    },
-    async unlockWallet() {
-      const password = await keytar.getPassword(
-        SERVICE_VERTO,
-        this.wallet.name
-      );
-      return this.invoke(
-        "/v1/wallet/unlock",
-        JSON.stringify([this.wallet.name, password])
-      );
-    },
-    async createKeys() {
-      return this.invoke(
-        "/v1/wallet/create_key",
-        JSON.stringify([this.wallet.name, "K1"])
-      );
-    },
-    async invoke(name, data) {
-      const url = this.eosUrl + name;
-      this.addMessage(`Calling "${url}"` + (data ? `(${data})` : "") + " ...");
-      try {
-        const resp = await this.$axios.$post(url, data);
-        this.addMessage(JSON.stringify(resp));
-        return resp;
-      } catch (e) {
-        const response = e.response;
-        this.addMessage(
-          "ERROR " +
-            response.status +
-            ": " +
-            JSON.stringify(response.data.error)
-        );
-
-        // TODO log error
-        throw e;
-      }
-    },
-    addMessage(message) {
-      this.messages += message + "\n";
-      this.scrollToEnd();
-    },
-    scrollToEnd() {
-      // FIXME I think the height is calculated before the message is added, and so it doesn't
-      // actually scroll to the correct place
-      // var messageBox = this.$el.querySelector("#messages");
-      // console.log("scrollHeight", messageBox.scrollHeight);
-      // messageBox.scrollTop = messageBox.scrollHeight + 10;
-    },
-    saveEosUrl() {
-      store.set(KEY_EOS_URL, this.eosUrl.trim());
-      console.log("Set new EOS URL to " + store.get(KEY_EOS_URL));
+  created() {
+    if (this.is_login) {
+      this.$router.push({ path: "home" });
+    } else {
+      this.$router.push({ path: "welcome" });
     }
   }
 };
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
 </script>
-
-<style scoped>
-.fixed-footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
-
-.code {
-  font-family: "Courier New", Courier, monospace;
-  font-size: small;
-}
-</style>
---
+++ pages/welcome.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="container is-fluid is-flex wallet-background is-centered has-text-centered">
+      <div class="main-section has-text-white">
+        <div class="welcome-section">WELCOME TO</div>
+        <div class="logo p-b-sm">
+          <img src="~/assets/img/verto-logo-white.png" >
+          <div class="is-size-5 has-text-weight-bold is-uppercase has-text-right">BY VOLENTIX</div>
+        </div>
+        <div class="p-t-md p-b-lg">{{ subtitle_message }}</div>
+        <div>{{ join_message }}</div>
+      </div>
+      <div class="create-wallet-btn-wrapper" >
+        <a class="button is-primary is-fullwidth create-wallet-btn">Create Your Wallet</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subtitle_message:
+        "Verto is your gateway to Volentix, with Verto, you can purchase VTX, the native token of the Volentix ecosystem.",
+      join_message: "We're glad you've joined us."
+    };
+  }
+};
+</script>
+
+<style scoped>
+.wallet-background {
+  height: 100vh;
+  background-image: url(~/assets/img/wallet-bg.jpeg);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+}
+
+/* Welcome Box Style */
+.main-section {
+  padding: 15px 10px 35px;
+  background: rgba(41, 41, 41, 0.6);
+  margin: 0 25px;
+  border-radius: 3px;
+  align-items: center;
+}
+.main-section .welcome-section {
+  text-transform: uppercase;
+  font-size: 2.8rem;
+  line-height: 3.5rem;
+}
+.main-section .logo {
+  padding-right: 40px;
+  padding-left: 40px;
+}
+.main-section .logo img {
+  width: 100%;
+}
+.main-section .logo div {
+  color: #97a4b2;
+  margin-top: -8px;
+  padding: 0 8px;
+}
+
+/* Create Wallet Button Style  */
+.create-wallet-btn-wrapper {
+  position: fixed;
+  bottom: 50px;
+  padding: 0 25px;
+  width: 100%;
+}
+.create-wallet-btn {
+  padding-top: 25px;
+  padding-bottom: 28px;
+  border-radius: 10px;
+  font-size: 14px;
+  background-color: #00a5a5;
+}
+</style>
