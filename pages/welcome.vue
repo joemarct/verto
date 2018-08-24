3354a728141d4392aa8ef29d11d13e2e42f2d02e
Omaro Taylor <smart.omarodev@gmail.com>
2018-08-24 13:38:59 -0500

    Update wallet screen template

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>
-    <div class="container is-fluid is-flex wallet-background is-centered has-text-centered">
-      <div class="main-section has-text-white">
-        <div class="welcome-section is-size-1">WELCOME TO</div>
-        <div class="logo p-b-sm">
-          <img src="~/assets/img/verto-logo-white.png" >
-          <div class="is-size-5 has-text-weight-bold is-uppercase has-text-right">BY VOLENTIX</div>
+  <section class="hero is-fullheight wallet-background">
+    <div class="hero-head">
+      <div class="container is-invisible">
+        <div class="level is-mobile p-lg">
+          <div class="level-item has-text-centered is-marginless">
+            <a class="p-t-lg p-b-lg button is-fullwidth is-primary">&nbsp;</a>
+          </div>
         </div>
-        <div class="p-t-md p-b-lg">{{ subtitle_message }}</div>
-        <div>{{ join_message }}</div>
-      </div>
-      <div class="create-wallet-btn-wrapper" >
-        <a class="button is-primary is-fullwidth create-wallet-btn">Create Your Wallet</a>
       </div>
     </div>
-  </div>
+    <div class="hero-body is-paddingless">
+      <div class="container p-l-lg p-r-lg has-text-centered">
+        <div class="box has-text-white is-radiusless is-shadowless">
+          <div class="is-size-1">WELCOME TO</div>
+          <div class="logo p-b-sm p-l-lg p-r-lg">
+            <img src="~/assets/img/white-logo-with-text.png" >
+          </div>
+          <div class="p-t-md p-b-lg is-size-5">{{ subtitle_message }}</div>
+          <div class="is-size-5 p-b-sm">{{ join_message }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="hero-foot">
+      <div class="container p-b-md">
+        <div class="level is-mobile p-lg">
+          <div class="level-item has-text-centered is-marginless">
+            <a class="p-t-lg p-b-lg button is-fullwidth is-primary">Create Your Wallet</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -35,47 +51,17 @@ export default {
   background-image: url(~/assets/img/wallet-bg.jpeg);
   background-size: 100% 100%;
   background-repeat: no-repeat;
-  align-items: center;
 }
-
-/* Welcome Box Style */
-.main-section {
-  padding: 15px 10px 35px;
-  background: rgba(41, 41, 41, 0.3);
-  margin: 0 25px;
-  border-radius: 3px;
-  align-items: center;
+.box {
+  background-color: rgba(0, 0, 0, 0.3);
 }
-.main-section .welcome-section {
-  text-transform: uppercase;
+.button {
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  border-radius: 0.5rem;
 }
-.main-section .logo {
-  padding-right: 40px;
-  padding-left: 40px;
+.logo img {
+  width: 100%;
   max-width: 320px;
-  margin: auto;
-}
-.main-section .logo img {
-  width: 100%;
-}
-.main-section .logo div {
-  color: #97a4b2;
-  margin-top: -8px;
-  padding: 0 8px;
-}
-
-/* Create Wallet Button Style  */
-.create-wallet-btn-wrapper {
-  position: fixed;
-  bottom: 50px;
-  padding: 0 25px;
-  width: 100%;
-}
-.create-wallet-btn {
-  padding-top: 25px;
-  padding-bottom: 28px;
-  border-radius: 10px;
-  font-size: 14px;
-  background-color: #00a5a5;
 }
 </style>
