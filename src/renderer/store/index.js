827d10eb08bc2a4c523c9a82766439b15034ec7b
Joemar Taganna <joemar.ct@gmail.com>
2018-10-30 18:59:17 +0800

    Persist vuex through local storage to survive refresh

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 // import modules from './modules'
 
@@ -7,6 +8,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   // strict: process.env.NODE_ENV !== 'production',
+  plugins: [createPersistedState()],
   state: {
     userKey: "",
     loggedin: false
