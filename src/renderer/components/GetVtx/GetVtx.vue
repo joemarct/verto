20a8bce41fe381f6d5811ec074849d1bc14b8faa
realrhys <rhys@relatid.com>
2018-12-05 11:39:14 -0500

    Updating the build

--- src/renderer/components/GetVtx/GetVtx.vue
+++ src/renderer/components/GetVtx/GetVtx.vue
@@ -3,7 +3,7 @@
     <div class="hero-head p-t-sm">
       <div class="control p-md has-text-centered">
         
-        <img v-if="!doneCountdown" src="~@/assets/img/ethereum.svg" class="logo">
+        <img v-if="!doneCountdown" src="~@/assets/img/ethereum.png" class="logo">
         <div class="getvtx-header">
           {{ $t('GetVtx.getvtx') }} {{ nativeChainName }}
         </div>
